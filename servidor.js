0c25a2390fabbc3e9c850bd3f9873324eb27201c
MANUELLA DE SOUZA SILVA <26171665@edu119.local>
2026-09-17 11:11:16 -0300

    salvando

--- servidor.js
+++ servidor.js
@@ -1,26 +1,43 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
-let ALUNOS = [
-    { id: 1, nome: "Bernardo", curso: "Desenvolvimento de Sistemas" },
-    { id: 2, nome: "Camily", curso: "Redes de computadores" },
-    { id: 3, nome: "Kaue", curso: "Banco de Dados" },
-    { id: 4, nome: "Maria", curso: "Administração" },
-    { id: 5, nome: "Marjory", curso: "Desenvolvimento de Sistemas" }
+let alunos = [
+    {id: 1, nome: "Malu", curso: "Desenvolvimento de Sistemas"},
+    {id: 2, nome: "Camily", curso: "Redes de computadores"},
+    {id: 3, nome: "Kaue", curso: "Banco de dados"},
+    {id: 4, nome: "Rian", curso: "Administração"},
+    {id: 5, nome: "Manu", curso: "Desenvolvimento de Sistemas"},
+]
+app.get("/", (req, res) => {
+  res.json({
+    message: "API Alunos funcionando"
+  });
+});
 
-];      
+app.get("/alunos", (req, res) => {
+    res.json(alunos);
+});
 
-app.get("/",(req,res)=>{
-    res.json({ALUNOS})
-        mensagem: "API Alunos funcionando"
-    })
+app.post("/alunos/cadastrar", (req, res) => {
+        //console.log(req.body);
+        const {nome, curso} = req.body;
+       //console.log("nome: " + nome);
+       //console.log(`curso: ${curso}`);
 
+       const novoAluno = {
+        nome : nome,
+        curso : curso,
+       }
+
+       alunos.push(novoAluno);
+
+       res.status(201).json({msg: "Aluno cadastrado com sucesso."});
+});
 
 const PORTA = 3000;
-app.listen(PORTA,()=>{
-     console.log("Servidor iniciado com sucesso");
-     console.log(`http://localhost:${PORTA}`);
-
-    })  
-
+app.listen(PORTA, () => {
+    console.log("Servidor iniciado com sucesso.");
+    console.log(`http://localhost:${PORTA}`);
+})
