a4cd41ead597386ac6cf228af69f6f5f5a554fc7
MANUELLA DE SOUZA SILVA <26171665@edu119.local>
2026-09-17 13:39:19 -0300

    Avaliar

--- servidor.js
+++ servidor.js
@@ -5,10 +5,10 @@ app.use(express.json());
 
 let alunos = [
     {id: 1, nome: "Malu", curso: "Desenvolvimento de Sistemas"},
-    {id: 2, nome: "Camily", curso: "Redes de computadores"},
+    {id: 2, nome: "Kemily", curso: "Redes de computadores"},
     {id: 3, nome: "Kaue", curso: "Banco de dados"},
     {id: 4, nome: "Rian", curso: "Administração"},
-    {id: 5, nome: "Manu", curso: "Desenvolvimento de Sistemas"},
+    {id: 5, nome: "Uendel", curso: "Desenvolvimento de Sistemas"},
 ]
 app.get("/", (req, res) => {
   res.json({
@@ -26,7 +26,14 @@ app.post("/alunos/cadastrar", (req, res) => {
        //console.log("nome: " + nome);
        //console.log(`curso: ${curso}`);
 
+       if(!nome || !curso){
+        return res.status(400).json({msg: "Nome e curso são obrigatórios."});
+       }
+
+       const id = alunos.length > 0 ? alunos[alunos.length - 1].id + 1 : 1;
+
        const novoAluno = {
+        id : id,
         nome : nome,
         curso : curso,
        }
@@ -34,7 +41,24 @@ app.post("/alunos/cadastrar", (req, res) => {
        alunos.push(novoAluno);
 
        res.status(201).json({msg: "Aluno cadastrado com sucesso."});
+
 });
+
+app.get("/alunos/:valor", (req, res) => {
+    const valor = req.params.valor;
+    const aluno = alunos.find((aluno) => aluno.id == valor || aluno.nome == valor);
+
+    res.status(200).json(aluno);
+});
+
+app.put("alunos/:valor", (req, res) =>{
+    const valor = req.params.id;
+    const {nome, curso} = req.body;
+
+    if(!nome || !curso){
+        return res.status(400).json({msg: "Nome e curso são obrigatórios."});
+    }
+})
 
 const PORTA = 3000;
 app.listen(PORTA, () => {
