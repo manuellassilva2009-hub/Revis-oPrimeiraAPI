c05cecd8aca6024f2c99c6758e1b73327d8762d0
MANUELLA DE SOUZA SILVA <26171665@edu119.local>
2026-09-17 09:51:09 -0300

    Salvando

---
+++ servidor.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+let ALUNOS = [
+    { id: 1, nome: "Bernardo", curso: "Desenvolvimento de Sistemas" },
+    { id: 2, nome: "Camily", curso: "Redes de computadores" },
+    { id: 3, nome: "Kaue", curso: "Banco de Dados" },
+    { id: 4, nome: "Maria", curso: "Administração" },
+    { id: 5, nome: "Marjory", curso: "Desenvolvimento de Sistemas" }
+
+];      
+
+app.get("/",(req,res)=>{
+    res.json({
+        mensagem: "API Alunos funcionando"
+    })
+})
+
+const PORTA = 3000;
+app.listen(PORTA,()=>{
+     console.log("Servidor iniciado com sucesso");
+     console.log(`http://localhost:${PORTA}`);
+
+    })  
+
